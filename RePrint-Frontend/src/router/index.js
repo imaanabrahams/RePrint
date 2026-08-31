e7aba4cd064db9142231aae9c058615224231676
Newmi-Git <yaghyaabdul1006@gmail.com>
2026-08-31 12:54:19 +0200

    added the login page with dummy logic

--- RePrint-Frontend/src/router/index.js
+++ RePrint-Frontend/src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/staff/login',
+      name: 'EmployeeLogin',
+      component: () => import('@/views/employeeLogin.vue'),
+    },
+  ],
 })
 
 export default router
